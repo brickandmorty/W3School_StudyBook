2674f3c042078c4032ef1522d2f3c69474d02bed
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-12-12 22:37:29 +0100

    Too much to learn to stop now. :-)

--- src/MethodParameters.java
+++ src/MethodParameters.java
@@ -1,16 +1,23 @@
 public class MethodParameters {
 
         static void myMethod(String fname, int age) {
-            System.out.println(fname + " is " + age);
+            System.out.println(fname + " is " + age + ".");
+        }
+
+        static int newMethod(int x) {
+            return 5+x;
+
+            // Outputs 8 (5 + 3)
         }
 
         public static void main(String[] args) {
+
+            System.out.println(newMethod(3));
+            System.out.println();
+
             myMethod("Liam", 5);
             myMethod("Jenny", 8);
             myMethod("Anja", 31);
         }
-    }
 
-// Liam is 5
-// Jenny is 8
-// Anja is 31
+    }
