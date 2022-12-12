ae59227701df8c490572309c26501fc78ffc5c9f
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-12-12 23:12:08 +0100

    Now we overload one method, instead.

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -1,5 +1,6 @@
 public class MethodOverloading {
 
+    /*
     static int plusMethodInt(int x, int y) {
         return x + y;
     }
@@ -14,4 +15,26 @@ public class MethodOverloading {
         System.out.println("int: " + myNum1);
         System.out.println("double: " + myNum2);
     }
+} */
+
+    // Instead of defining two methods that should do the same thing, it is better to overload one.
+    // In the example below, we overload the plusMethod method to work for both int and double:
+
+    static int plusMethod(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethod(double x, double y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+    }
 }
+
+
+
