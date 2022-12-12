200af8e1ba6a7ff408711d7960ebf80a75ef497b
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-12-12 22:47:55 +0100

    Another example added.

--- src/MethodParameters.java
+++ src/MethodParameters.java
@@ -10,7 +10,22 @@ public class MethodParameters {
             // Outputs 8 (5 + 3)
         }
 
+        static int Addition(int x, int y) {
+            return x+y;
+        }
+
+        static int StoringResultInVariable (int x, int y){
+            return x*y;
+        }
+
         public static void main(String[] args) {
+
+            System.out.println(Addition(15,21));
+            System.out.println();
+
+            int z = StoringResultInVariable(10,4);
+            System.out.println(z);
+            System.out.println();
 
             System.out.println(newMethod(3));
             System.out.println();
