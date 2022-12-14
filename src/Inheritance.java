55f0f3e42cb735f0fe3c87ab60c562aceb18aa7d
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-12-14 21:23:52 +0100

    Polymorphism explained

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -1,7 +1,6 @@
 public class Inheritance {
 
     // exemplified with Vehicles
-
     protected String brand = "Ford";
     public void honk() {
         System.out.println("My car goes \"Tuut tuut!\" :D");
