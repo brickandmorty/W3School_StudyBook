98f85890931c3610894c0ea5f5729cdd932fb691
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-12-14 17:15:48 +0100

    Kurzzeitig Volatil. Part 1.

--- src/AccessModifiers.java
+++ src/AccessModifiers.java
@@ -27,4 +27,16 @@ methods and constructors. We divide modifiers into two groups:
     final           The class cannot be inherited by other classes
     abstract        The class cannot be used to create objects (To access an abstract class, it must be inherited from another class.) */
 
+    /* For Attributes and Methods:
+    final           Attributes and methods cannot be overridden/modified
+    static          Attributes and methods belongs to the class, rather than an object
+    abstract        Can only be used in an abstract class, and can only be used on methods. The method does not have a body, for example abstract void run();.
+                    The body is provided by the subclass (inherited from).
+    transient       Attributes and methods are skipped when serializing the object containing them
+    synchronized    Methods can only be accessed by one thread at a time
+    volatile        The value of an attribute is not cached thread-locally, and is always read from the "main memory"
+
+
+     */
+
 }
